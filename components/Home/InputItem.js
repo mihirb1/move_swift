cf3ed9fd14ece3e84e4b665e99ad9dd83d8756ed
mihirb1 <mihirborkar2004@gmail.com>
2024-10-16 18:33:06 -0700

    First commit

---
+++ components/Home/InputItem.js
@@ -0,0 +1,89 @@
+"use client"
+import { SourceContext } from '../../context/SourceContext';
+import { DestinationContext } from '../../context/DestinationContext';
+import Image from 'next/image'
+import React, { useContext, useEffect, useState } from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
+
+function InputItem({type}) {
+    const [value, setValue] = useState(null);
+    const [placeholder, setPlaceholder] = useState(null);
+    const {source,setSource}=useContext(SourceContext);
+    const {destination,setDestination}=useContext(DestinationContext);
+    useEffect(()=>{
+        type=='source'
+        ?setPlaceholder('Pickup Location')
+        :setPlaceholder('Dropoff Location')
+    },[]);
+
+    const getLatAndLng=(place,type)=>{
+       const placeId=place.value.place_id;
+       const service=new google.maps.places.PlacesService(document.createElement('div'));
+       service.getDetails({placeId},(place,status)=>{
+        if(status==='OK' && place.geometry && place.geometry.location)
+        {
+            console.log(place.geometry.location.lng());
+            if(type=='source')
+            {
+              setSource({
+                lat:place.geometry.location.lat(),
+                lng:place.geometry.location.lng(),
+                name:place.formatted_address,
+                label:place.name
+              })
+            }else{
+              console.log("Des")
+              setDestination({
+                lat:place.geometry.location.lat(),
+                lng:place.geometry.location.lng(),
+                name:place.formatted_address,
+                label:place.name
+              })
+            }
+        }
+       })
+    }
+  return (
+    <div className='bg-slate-200 p-3 px-3 rounded-lg mt-6 
+    flex items-center gap-4'>
+        <Image src={type=='source'?'/source.png':'/dest.png'} width={15} height={15}/>
+      
+         <GooglePlacesAutocomplete 
+          selectProps={{
+            value,
+            onChange: (place)=>{getLatAndLng(place,type);
+                setValue(place)},
+            placeholder:placeholder,
+            isClearable:true,
+            className:'w-full',
+            components:{
+                DropdownIndicator:false
+            },
+            styles: {
+              control: (provided) => ({
+                  ...provided,
+                  backgroundColor: '#00ffff00',
+                  border: 'none'
+              }),
+              singleValue: (provided) => ({
+                  ...provided,
+                  color: 'black' // Ensures the selected value is black
+              }),
+              option: (provided, state) => ({
+                  ...provided,
+                  color: state.isSelected ? 'white' : 'black', // Dropdown options will be black
+                  backgroundColor: state.isSelected ? '#00aaff' : 'white'
+              }),
+              input: (provided) => ({
+                  ...provided,
+                  color: 'black' // Ensures the input text is black
+              })
+            }
+          }}
+         />
+
+    </div>
+  )
+}
+
+export default InputItem
---
+++ utils/CarListData.js
@@ -0,0 +1,40 @@
+export const CarListData = [
+    {
+        id: 1,
+        name: 'Standard Delivery',
+        desc: 'Affordable, reasonable shipping',
+        amount: 1.1,
+        image: '/standard.png'
+    },
+    {
+        id: 2,
+        name: 'Large Delivery',
+        desc: 'For oversized packages',
+        amount: 1.6,
+        image: '/large.png'
+    },
+    {
+        id: 3,
+        name: 'Expedited Delivery',
+        desc: 'Same-day delivery guaranteed',
+        amount: 1.9,
+        image: '/expedite.png'
+    },
+    {
+        id: 4,
+        name: 'Safe Delivery',
+        desc: 'Ensure safe handling',
+        amount: 1.4,
+        image: '/safe.png'
+    },
+    {
+        id: 5,
+        name: 'Scheduled Delivery',
+        desc: 'Select a time frame for your needs',
+        amount: 1.3,
+        image: '/scheduled.png'
+    },
+
+
+
+] 
